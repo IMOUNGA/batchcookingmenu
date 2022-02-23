bfb2f47d22350f790368fd08ad32e3606039cd65
romann <romann.imounga@cinego.net>
2022-02-23 12:40:57 +0100

    create component, html and scss for the component shape

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { NouveautesComponent } from './home/nouveautes/nouveautes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    NouveautesComponent,
   ],
   imports: [
     BrowserModule,
