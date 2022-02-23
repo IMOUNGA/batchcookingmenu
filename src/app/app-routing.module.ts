8ca5f8e05e391c1c519285f836f379ed7cc3bb07
romann <romann.imounga@cinego.net>
2022-02-23 10:08:32 +0100

    header component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
 
 const routes: Routes = [
   {
+    path: '',
+    component: HomeComponent,
+  },
+  /*{
     path: 'pages',
     children: [
       {
@@ -10,7 +15,7 @@ const routes: Routes = [
         loadChildren: () => import('./pages-layout/pages-layout.module').then(m => m.PagesLayoutModule),
       }
     ]
-  }
+  }*/
 ];
 
 @NgModule({
