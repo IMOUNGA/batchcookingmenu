9a1170a7f4069984dda79fac2725e96915be9c47
romann <romann.imounga@cinego.net>
2022-02-28 16:12:30 +0100

    add emailjs

--- src/app/shared/forms/newsletter/newsletter.component.ts
+++ src/app/shared/forms/newsletter/newsletter.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {EmailjsService} from "../../../services/emailjs.service";
 
 @Component({
   selector: 'app-newsletter',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./newsletter.component.scss']
 })
 export class NewsletterComponent implements OnInit {
+  @ViewChild('userName') userName: ElementRef | undefined;
+  @ViewChild('userEmail') userEmail: ElementRef | undefined;
 
-  constructor() { }
+
+  constructor(private email: EmailjsService) { }
 
   ngOnInit(): void {
+  }
+
+  subscribeCustomer(): void {
+    const email = this.userEmail?.nativeElement.value;
+    const name = this.userName?.nativeElement.value;
+
+
+    if (email.length > 0 && name.length > 0) {
+      const validateEmailRegex = /^\S+@\S+\.\S+$/;
+
+      if (email.match(validateEmailRegex)) {
+        console.log(("c'est ok"))
+        //const result = this.email.send(name, email);
+      } else {
+        console.error('Mail value is not a mail');
+      }
+    }
   }
 
 }
