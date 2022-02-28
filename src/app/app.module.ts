fbfec2ba95b33ac5ce13a4aa744a67e52a7c255f
romann <romann.imounga@cinego.net>
2022-02-28 14:35:55 +0100

    coming soon component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NouveautesComponent } from './home/nouveautes/nouveautes.component';
+import { ComingSoonComponent } from './coming-soon/coming-soon.component';
+import {ComingSoonModule} from "./coming-soon/coming-soon.module";
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NouveautesComponent,
+    ComingSoonComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ComingSoonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
